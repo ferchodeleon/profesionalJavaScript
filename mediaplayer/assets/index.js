4a04e77c85d0e643878a5b06c62ed3dd3afc9426
Fernando De Leon <fdeleon@lacadenaretail.com>
2020-12-17 09:54:18 -0500

    add project mediaPlayer

--- mediaplayer/assets/index.js
+++ mediaplayer/assets/index.js
@@ -1,12 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 // const player = new MediaPlayer({ el: video });
-const player = new MediaPlayer({ 
+const player = new MediaPlayer({
   el: video,
   plugins: [
-    new AutoPlay()
+    new AutoPlay(),
+    new AutoPause(),
   ],
 });
 
@@ -15,3 +17,9 @@ button.onclick = () => player.togglePlay();
 
 const buttonMute = document.getElementById("mute")
 buttonMute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
